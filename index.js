59f4c953ae39166637261c2f9ac7ad1fb57b27cc
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-06-28 23:51:48 -0700

    moving transform-bind-attr-to-attributes to setupPreprocessRegistry

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-/* jshint node: true */
+/*jshint node: true */
 'use strict';
 
 module.exports = {
-  name: 'bind-attr'
+  name: 'bind-attr',
+
+  setupPreprocessorRegistry: function(type, registry) {
+    var TransformBindAttrToAttributes = require('./lib/ember-template-compiler/plugins/transform-bind-attr-to-attributes');
+
+    registry.add('htmlbars-ast-plugin', {
+      name: 'transform-bind-attr-to-attributes',
+      plugin: TransformBindAttrToAttributes
+    });
+  }
 };
