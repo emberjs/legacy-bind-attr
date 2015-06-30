8910f54b4df917d5db1167a56c26fffcf5209e70
Jason Mitchell <jasonmit@yahoo-inc.com>
2015-06-29 17:31:25 -0700

    Relying on resolver & noop plugin when > beta.1 since bind-atto will be removed in beta.2
    fixes #2 #3

--- app/initializers/register-bind-attr-class.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import BindAttrClassHelper from 'bind-attr/helpers/-bind-attr-class';
-
-export default {
-  name: 'register-bind-attr-class',
-  initialize: function(/*registry, app*/) {
-    Ember.HTMLBars._registerHelper('-bind-attr-class', BindAttrClassHelper);
-  }
-}
--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 /*jshint node: true */
 'use strict';
 
+var VersionChecker = require('ember-cli-version-checker');
+var triggered = false;
+
 module.exports = {
   name: 'bind-attr',
 
   setupPreprocessorRegistry: function(type, registry) {
-    var TransformBindAttrToAttributes = require('./lib/ember-template-compiler/plugins/transform-bind-attr-to-attributes');
+    var checker = new VersionChecker(this);
+    this._checkerForEmber = checker.for('ember', 'bower');
 
-    registry.add('htmlbars-ast-plugin', {
-      name: 'transform-bind-attr-to-attributes',
-      plugin: TransformBindAttrToAttributes
-    });
+    if (this._checkerForEmber.gt('2.0.0-beta.1')) {
+      var TransformBindAttrToAttributes = require('./lib/ember-template-compiler/plugins/transform-bind-attr-to-attributes');
+
+      registry.add('htmlbars-ast-plugin', {
+        name: 'transform-bind-attr-to-attributes',
+        plugin: TransformBindAttrToAttributes
+      });
+    } else {
+      if (!triggered) {
+        console.warn('legacy-bind-attr is not required for Ember 2.0.0-beta.1 and earlier, please remove from your `package.json`.');
+        triggered = true;
+      }
+    }
   }
 };
